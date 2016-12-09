ac129aaacbff882b2ea1db7fcf3648b62847b364
Rafael <rapha.pse@outlook.com>
2016-12-08 22:29:10 -0200

    Primeiros passos com Ruby

---
+++ ruby001/ruby001_variaveis.rb
@@ -0,0 +1,17 @@
+a = "Rafael" #string
+b = 10 #int
+c = true #booleano
+
+puts a
+puts b
+puts c
+
+puts"------Tipos das varaveis-----------"
+puts a.class
+puts b.class
+puts c.class
+
+idade = 30
+multiplicador = 2
+
+puts idade * multiplicador
