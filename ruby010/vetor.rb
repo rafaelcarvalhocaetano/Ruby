123dda8229891f391acaa8f3a35a2538b6e3d72f
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-10 15:56:56 -0200

    Vetores

---
+++ ruby010/vetor.rb
@@ -0,0 +1,16 @@
+puts"---atribuindo valores para o vetor------"
+v = [1,2,3,4]
+puts v.class
+puts v.inspect
+puts v
+puts"-----empurando valores para o vetor--------"
+a = Array.new
+a.push(4)
+puts a.inspect
+puts"------ARRAY ARGV-------"
+x = ARGV
+puts x.inspect
+puts x
+puts"-------SOMA DE VETORES-----"
+d = ARGv[0].to_i + ARGV[1].to_i
+puts d
