176389dd835f917eb2d98e3447f2f424bcbd1b4b
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-10 13:27:30 -0200

    Operadores Aritméticos

---
+++ ruby008/operadoresAritmeticos.rb
@@ -0,0 +1,11 @@
+a = 2
+puts "A =",a
+b = 3
+puts "B =",b
+puts "a + b = ", a + b
+puts"a - b = ", a - b
+puts "a * b = ", a * b
+puts "a / b = ", a * b
+puts "a ** b = ", a ** b
+puts "b % a", a % b
+
