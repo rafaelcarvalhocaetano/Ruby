55e3f3c53dfad880dca29c8eff8f3ad1d42d5b1a
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-10 13:27:05 -0200

    Operadores Relacionais

---
+++ ruby007/operadoresRelacionais.rb
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+c = 3
+puts "A == ",a
+puts "B == ",b
+puts "C == ",c
+if a < b
+  puts"a < b"
+else
+  puts"a > b"
+end
+puts"---igualdade ( == )---"
+if a == b
+  puts"A não é igual a B"
+else
+  puts"A é igual a B"
+end
+puts"---diferente---"
+unless a != b
+  puts"Verdade"
+else
+  puts"Falso"
+end
