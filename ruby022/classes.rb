2c782244af44db04a398cc227a9d426a02e001c2
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-14 16:19:23 -0200

    Classes

---
+++ ruby022/classes.rb
@@ -0,0 +1,9 @@
+a = [1,2,3,4]
+puts a
+b = Array.new
+puts b
+
+puts"------------"
+
+b.push(5,5,5)
+puts b
---
+++ ruby022/criando.rb
@@ -0,0 +1,8 @@
+class Pessoas
+  def apresentar()
+    puts "Eu sou uma class"
+  end
+  def numero()
+    puts self.object_id
+  end
+end
